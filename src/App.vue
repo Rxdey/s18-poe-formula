3374fdd9f6cb39113ec427c4c5d3a5580a753064
rxdey <jyh1994@qq.com>
2022-02-14 17:22:21 +0800

    feat: 调整样式

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="page-wrap"></div>
+  <div class="page-wrap">
+    <section class="items-wrap">
+      <div class="items-list">
+        <div class="items-card" v-for="(item, i) in items" :key="i">
+
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, computed } from 'vue';
+import { data } from '@/data/data';
+
+const items = ref(data);
 
 </script>
 
 <style lang="less">
-html,
-body,
-#app {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  overflow: hidden;
-}
+@import './App.less';
 </style>
